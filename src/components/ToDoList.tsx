6a9851937278b9a552157e92cb3980151f32f8e5
bauer-hillary-c <hbauer@ezcater.com>
2019-04-27 15:16:34 -0400

    Adds useState hook and handleSubmit (does not persist value)

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+
+type FormElem = React.FormEvent<HTMLFormElement>;
 
 const ToDoList: React.SFC<{}> = () => {
+  const [value, setValue] = useState<string>('')
+
+  const handleSubmit = (e: FormElem):void => {
+    e.preventDefault()
+    setValue('')
+  }
   return (
   <Fragment>
     <h1>Todo List</h1>
     <form>
-      <input type='text' required />
+      <input
+        type='text'
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        required
+      />
       <button type='submit'>Add Todo</button>
     </form>
   </Fragment>
