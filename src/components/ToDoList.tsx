bccf4029882a314209c93dbe844bd8c06e2424c5
bauer-hillary-c <hbauer@ezcater.com>
2019-04-27 15:25:49 -0400

    Saves new todos in state

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,26 +2,40 @@ import React, { Fragment, useState } from 'react'
 
 type FormElem = React.FormEvent<HTMLFormElement>;
 
+interface ToDoInterface {
+  text: string
+  complete: boolean
+}
+
 const ToDoList: React.SFC<{}> = () => {
   const [value, setValue] = useState<string>('')
+  const [todos, setTodos] = useState<ToDoInterface[]>([])
 
   const handleSubmit = (e: FormElem):void => {
     e.preventDefault()
+    addToDo(value)
     setValue('')
   }
+
+  const addToDo = (text: string) => {
+    const newToDos: ToDoInterface[] = [...todos, {text, complete: false}]
+    setTodos(newToDos)
+  }
+
+    console.log(todos)
   return (
-  <Fragment>
-    <h1>Todo List</h1>
-    <form>
-      <input
-        type='text'
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        required
-      />
-      <button type='submit'>Add Todo</button>
-    </form>
-  </Fragment>
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
+    </Fragment>
   )
 }
 
